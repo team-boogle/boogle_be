e915c5b44ff0d3bb31315de0852ca14cc7cec4a9
sseongyunn <sseongyunn@korea.ac.kr>
2025-05-18 17:59:58 +0900

    comment: 로그인 API 테스트 방법 주석 추가

--- test/login.test.js
+++ test/login.test.js
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import app from '../src/server.js'; // 서버 객체 import
 
+//npm run test 로 테스트 가능
 describe('로그인 API 테스트', () => {
     it('성공: 올바른 아이디와 비밀번호로 로그인', async () => {
         const response = await request(app)
