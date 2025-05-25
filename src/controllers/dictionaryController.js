b5c319b800d214882f04203ff4a2a0495569efb3
gun2jang <firstunderbar@gmail.com>
2025-05-26 01:36:32 +0900

    reqtest

--- src/controllers/dictionaryController.js
+++ src/controllers/dictionaryController.js
@@ -6,6 +6,7 @@ function setDictionary(set) {
 
 function checkWord(req, res) {
   const { word } = req.body;
+  console.log(word);
   if (!word) {
     return res.status(400).json({ success: false, message: 'require valid word.' });
   }
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const loadDictionary = require('./utils/loaddictionary');
 const dictionaryRoutes = require('./routes/dictionaryRoutes');
 const { setDictionary } = require('./controllers/dictionaryController');
@@ -6,6 +7,14 @@ const { setDictionary } = require('./controllers/dictionaryController');
 const app = express();
 app.use(express.json());
 
+const corsOptions = {
+  origin: 'http://localhost:3000', // Next.js 프론트엔드 주소
+  methods: ['POST', 'GET'],    // 허용할 HTTP 메소드
+  allowedHeaders: ['Content-Type'], // 허용할 요청 헤더
+  credentials: true // 만약 쿠키나 인증 헤더를 주고받는다면 필요
+};
+
+app.use(cors(corsOptions));
 // dictionary loading
 const dictionaryPath = './dictionary.txt';
 const wordSet = loadDictionary(dictionaryPath);
@@ -16,7 +25,7 @@ console.log(`${wordSet.size}개 단어 로딩 완료`);
 app.use('/', dictionaryRoutes);
 
 // server 
-const PORT = 3000;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`server ongoing: http://localhost:${PORT}`);
 });
