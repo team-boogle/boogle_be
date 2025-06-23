fa21bf2b72b416339a8faf2e0d7cc5071e774bfb
jasonwayn <harveylew8@gmail.com>
2025-06-23 16:28:28 +0900

    jwt to session

--- src/routes/signRoutes.js
+++ src/routes/signRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const verifyToken = require('../middleware/authMiddleware');
+const verifySession = require('../middleware/authMiddleware');
 const ctrl = require('../controllers/signController');
 
 
